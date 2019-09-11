dd924e7b12ae591fb05d4b398c32a95a1bf7c60b
Lucas Henrique Carlos Domingos <lucaa.henrique.carlos@gmail.com>
2019-09-11 08:38:06 -0300

    clean code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 
 import Child from './Components/Child';
 
-export class App extends Component {
-  state = {
-    step: 0,
-    dados: {}
-  };
-
-  handleContextStep = step => this.setState({ step });
-  handleContextData = data => {
-      const defaultData = this.state.dados;
-      console.log(data);
-
-      this.setState({ dados: {...defaultData, ...data} });
-  };
-
-  render() {
-      const contextValue = {
-          step: this.state.step,
-          handleContextStep: this.handleContextStep,
-          dados: this.state.dados,
-          handleContextData: this.handleContextData,
-      };
-
-
-      return (
-          <div className="App">
-            <Provider store={store}>
-              <Child/>
-            </Provider>
-          </div>
-      );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <Child/>
+      </Provider>
+    </div>
+  );
 }
-
-export default App;
